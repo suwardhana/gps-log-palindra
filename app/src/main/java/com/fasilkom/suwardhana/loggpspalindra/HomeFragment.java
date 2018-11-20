f430aadec8d7a8dd9d9ac3aa5a78152b4488e25d
Suwardhana <spensajambi@gmail.com>
2018-11-20 11:12:48 +0700

    implement fragment

---
+++ app/src/main/java/com/fasilkom/suwardhana/loggpspalindra/HomeFragment.java
@@ -0,0 +1,45 @@
+
+package com.fasilkom.suwardhana.loggpspalindra;
+
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * {@link Fragment} that displays a list of number vocabulary words.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.layout_home, container, false);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+}
